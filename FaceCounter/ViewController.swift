40254f6bd45900017e4e2b1c93d015c5332bb759
Leo Tsuchiya <leotsuchiya@Leos-MacBook-Pro.local>
2017-11-26 14:52:47 -1000

    Initial Commit

---
+++ FaceCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceCounter
+//
+//  Created by Leo Tsuchiya on 11/26/17.
+//  Copyright © 2017 Leo Tsuchiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
