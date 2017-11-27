43462c0975d7a28a81f321497b96f7c28d05507c
Leo Tsuchiya <leotsuchiya@Leos-MacBook-Pro.local>
2017-11-26 17:56:11 -1000

    video detection kinda working

--- FaceCounter/VideoCaptureViewController.swift
+++ FaceCounter/VideoCaptureViewController.swift
@@ -9,8 +9,11 @@
 import Foundation
 import UIKit
 import AVKit
+import Vision
 
-class VideoCaptureViewController: UIViewController, AVCaptureAudioDataOutputSampleBufferDelegate {
+class VideoCaptureViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    var redView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,15 +37,79 @@ class VideoCaptureViewController: UIViewController, AVCaptureAudioDataOutputSamp
         dataOutput.setSampleBufferDelegate(self as! AVCaptureVideoDataOutputSampleBufferDelegate, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
         
-        
-        
-        
+//        let testView = UIView()
+//        testView.backgroundColor = .blue
+//        testView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//        self.view.addSubview(testView)
         
         self.view.backgroundColor = UIColor.brown
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        print("Camera was able to capture a frame:", Date())
+//        print("Camera was able to capture a frame:", Date())
+        
+        guard let pixelBuffer:CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        let ciImage = CIImage(cvImageBuffer: pixelBuffer, options: [:])
+        
+        
+        let request = VNDetectFaceRectanglesRequest(completionHandler: { (vnrequest, error) in
+            
+            if let error = error {
+                print("VNRequest Error:", error)
+            }
+            else {
+                print("Good request:", vnrequest)
+                
+                if vnrequest.results?.count == 0 {
+                    DispatchQueue.main.async {
+                        self.redView.removeFromSuperview()
+                    }
+                }
+                
+                vnrequest.results?.forEach({ (result) in
+                    print("result:",result)
+                    
+                    guard let faceObservation = result as? VNFaceObservation else { return }
+                    
+                    
+                    DispatchQueue.main.async {
+//                        let redView = UIView()
+                        
+                        let x = self.view.frame.width * faceObservation.boundingBox.origin.x
+                        let height = self.view.frame.height * faceObservation.boundingBox.height
+                        
+                        let y = self.view.frame.height * faceObservation.boundingBox.origin.y
+                        
+                        let width = self.view.frame.width * faceObservation.boundingBox.width
+
+                        
+                        self.redView.backgroundColor = .red
+                        self.redView.alpha = 0.5
+                        self.redView.frame = CGRect(x: x, y: y, width: width, height: height)
+                        self.view.addSubview(self.redView)
+                        self.view.bringSubview(toFront: self.redView)
+                    }
+                    
+                    print(faceObservation.boundingBox)
+                })
+            }
+        })
+        
+//        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
+        let videoHandler = VNSequenceRequestHandler()
+        do {
+            try videoHandler.perform([request], on: ciImage)
+        } catch let handlerError {
+            print("Handler perform error:",handlerError)
+        }
+        
+        
+//        do {
+//            try handler.perform([request])
+//        } catch let handlerError {
+//            print("Handler perform error:",handlerError)
+//        }
+        
     }
 }
 
