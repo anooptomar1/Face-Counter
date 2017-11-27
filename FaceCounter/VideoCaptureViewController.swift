2a5fde5b939facb4c3164b966f0437bdeb428cc2
davidkneely <davidics613@gmail.com>
2017-11-26 17:08:29 -1000

    Commiting video changes

--- FaceCounter/VideoCaptureViewController.swift
+++ FaceCounter/VideoCaptureViewController.swift
@@ -8,15 +8,58 @@
 
 import Foundation
 import UIKit
+import AVKit
 
-
-class VideoCaptureViewController:UIViewController {
-    
+class VideoCaptureViewController: UIViewController, AVCaptureAudioDataOutputSampleBufferDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self as! AVCaptureVideoDataOutputSampleBufferDelegate, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        
+        
+        
+        
         self.view.backgroundColor = UIColor.brown
     }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("Camera was able to capture a frame:", Date())
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
